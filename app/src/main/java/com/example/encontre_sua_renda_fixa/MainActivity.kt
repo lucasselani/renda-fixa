1eb77ba352fe1e1e13b9637e7a31e8c970407340
Lucas Selani de Melo <lucas.selani@toodoo.com.br>
2019-03-09 16:13:09 -0300

    Initial project
    Signed-off-by: Lucas Selani de Melo <lucas.selani@toodoo.com.br>

---
+++ app/src/main/java/com/example/encontre_sua_renda_fixa/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.encontre_sua_renda_fixa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
